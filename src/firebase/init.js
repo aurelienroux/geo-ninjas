d1eea53aae271a817f6624822882167e4cf7e82b
Aurélien Roux <aureroux13@gmail.com>
2020-02-02 11:47:26 -0500

    env file

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: 'AIzaSyApiOkmYCnm4FvfFb4O_z2olUP2NSJL2p4',
-  authDomain: 'geo-ninjas-58398.firebaseapp.com',
-  databaseURL: 'https://geo-ninjas-58398.firebaseio.com',
-  projectId: 'geo-ninjas-58398',
-  storageBucket: 'geo-ninjas-58398.appspot.com',
-  messagingSenderId: '990284691693',
-  appId: '1:990284691693:web:834eaf83f3ecf296c2f4e4'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_API_ID
 }
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig)
