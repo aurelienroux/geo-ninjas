1c0d0b451af1c7457d3be65ee7171b661f79bf59
Aurélien Roux <aureroux13@gmail.com>
2020-02-08 12:33:49 -0500

    update env file

--- README.md
+++ README.md
@@ -1,24 +1,43 @@
 # geo-ninjas
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Firestore connection
+
+At project root, create a `.env` file with the following model
+
+```
+VUE_APP_FIREBASE_API_KEY=XXX
+VUE_APP_FIREBASE_AUTH_DOMAIN=XXX
+VUE_APP_FIREBASE_DATABASE_URL=XXX
+VUE_APP_FIREBASE_PROJECT_ID=XXX
+VUE_APP_FIREBASE_STORAGE_BUCKET=XXX
+VUE_APP_FIREBASE_MESSAGING_SENDER_ID=XXX
+VUE_APP_FIREBASE_API_ID=XXX
+```
--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_API_ID
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_API_ID
 }
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig)
