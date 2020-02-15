f7639f9ddde04439b32a037eb30805355335440c
Aurélien Roux <aureroux13@gmail.com>
2020-02-15 16:31:50 -0500

    create cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// eslint-disable-next-line
+exports.checkAlias = functions.https.onCall((data, context) => {
+  const ref = admin
+    .firestore()
+    .collection('users')
+    .doc(data.slug)
+
+  return (
+    ref
+      .get()
+      .then(doc => {
+        return { unique: !doc.exists }
+      })
+      // eslint-disable-next-line
+      .catch(err => {
+        throw new functions.https.HttpsError('failed to connect')
+      })
+  )
+})
