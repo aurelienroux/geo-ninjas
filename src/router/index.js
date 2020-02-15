cc3972e9940aba81459c20cefca14a99aaae384d
Aurélien Roux <aureroux13@gmail.com>
2020-02-15 14:36:10 -0500

    add a route guard to map page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import GMap from '@/components/home/GMap'
 import Signup from '@/components/auth/Signup'
 import Login from '@/components/auth/Login'
+import firebase from 'firebase'
 
 Vue.use(VueRouter)
 
@@ -10,7 +11,10 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: GMap
+    component: GMap,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/signup',
@@ -26,6 +30,24 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  // check to see if routes requires auth
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    //check auth state of user
+    let user = firebase.auth().currentUser
+
+    if (user) {
+      //user signed in, proceed to route
+      next()
+    } else {
+      // no user signed in, redirect to login
+      next({ name: 'login' })
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
