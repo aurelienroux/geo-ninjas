6ffd40889a6b8e3365efb23280b046cb5348896a
Aurélien Roux <aureroux13@gmail.com>
2020-02-15 14:11:39 -0500

    update user geolocation

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -5,7 +5,8 @@
 </template>
 
 <script>
-// import firebase from 'firebase'
+import firebase from 'firebase'
+import db from '@/firebase/init'
 
 export default {
   name: 'GMap',
@@ -30,12 +31,35 @@ export default {
   },
   mounted() {
     if (document.getElementById('map')) {
+      let user = firebase.auth().currentUser
+
+      // get user location
       if (navigator.geolocation) {
-        // get user location
         navigator.geolocation.getCurrentPosition(
           pos => {
             this.lat = pos.coords.latitude
             this.lng = pos.coords.longitude
+
+            // find the user record and then update records
+            db.collection('users')
+              .where('user_id', '==', user.uid)
+              .get()
+              .then(snapshot => {
+                snapshot.forEach(doc => {
+                  db.collection('users')
+                    .doc(doc.id)
+                    .update({
+                      geolocation: {
+                        lat: pos.coords.latitude,
+                        lng: pos.coords.longitude
+                      }
+                    })
+                })
+              })
+              .then(() => {
+                this.renderMap()
+              })
+
             this.renderMap()
           },
           err => {
